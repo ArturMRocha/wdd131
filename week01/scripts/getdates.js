60cc2159de30f0eab3fa88883e852032fbe5be9e
ArturMRocha <Nemezix9@outlook.com>
2024-11-15 00:26:02 -0300

    Delete week01 directory

--- week01/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// getdates.js
-
-// Atualiza o ano atual no rodapé
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Atualiza a data da última modificação no rodapé
-document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
